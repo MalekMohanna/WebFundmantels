ce1cbc6d0478ec2c10b8d2086430e02f5285a30c
MalekMohanna <malekmhana@gmail.com>
2022-08-30 13:12:22 +0300

    VideoPlayer

---
+++ JavaScript/Video Preview/script.js
@@ -0,0 +1,12 @@
+function player(element){
+    element.play();
+    element.muted=true;
+}
+
+function off1(element){
+    element.pause();
+}
+
+function whenclicked(element){
+    element.muted=false;
+}
