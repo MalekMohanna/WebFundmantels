602d07d1ad151c3f1124a340cdd41da90e1b57a4
MalekMohanna <malekmhana@gmail.com>
2022-08-31 08:42:00 +0300

    edit

--- JavaScript/Likes/script.js
+++ JavaScript/Likes/script.js
@@ -1,4 +1,18 @@
 function liked (){
     var x = document.getElementById("likes-num");
+    // var x = document.querySelector("span");
         x.innerHTML = (1)+(parseInt(x.innerHTML));
     }
+
+    function liked1 (){
+        var x = document.getElementById("likes-num1");
+        // var x = document.querySelector("span");
+            x.innerHTML = (1)+(parseInt(x.innerHTML));
+        }
+    
+        function liked2 (){
+            var x = document.getElementById("likes-num2");
+            // var x = document.querySelector("span");
+                x.innerHTML = (1)+(parseInt(x.innerHTML));
+            }
+        
