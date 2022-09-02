894203eccb8d17a522b64eb7c6c12c6a0f8299cb
MalekMohanna <malekmhana@gmail.com>
2022-09-02 14:43:22 +0300

    Update/change

--- JavaScript/DojoWeather/script.js
+++ JavaScript/DojoWeather/script.js
@@ -1,14 +1,41 @@
-function notif(){
-    let x=document.querySelector(".cities").innerHTML;
+function notif() {
+    let x = document.querySelector(".cities").innerHTML;
     alert("Loading weather report..");
 }
 
-function removeco(){
-    let y=document.querySelector("footer");
+function removeco() {
+    let y = document.querySelector("footer");
     y.remove();
 }
 
-function change(){
-    var x = document.querySelector(".measure").value;
-    document.getElementById(".low").innerHTML = x*(9/5) + 32;
+function change(element) {
+    var x = document.querySelectorAll(".high");
+    var y = document.querySelectorAll(".low");
+    console.log(element.value)
+    if (element.value == "℃") {
+        changeCtoF(x, y);
+    }
+    else {
+        changeFtoC(x, y);
+    }
 }
+
+function changeFtoC(x,y) {
+    for (var i = 0; i < x.length; i++) {
+        x[i].innerHTML = parseInt(parseInt(x[i].innerHTML) * (9 / 5) + 32);
+    } 0
+    for (var j = 0; j < y.length; j++) {
+        y[j].innerText = parseInt(parseInt(y[j].innerText) * (9 / 5) + 32);
+    }
+}
+
+
+function changeCtoF(x,y) {
+    for (var i = 0; i < x.length; i++) {
+        x[i].innerHTML = parseInt(parseInt((x[i].innerHTML) - 32) * (5 / 9));
+    } 0
+    for (var j = 0; j < y.length; j++) {
+        y[j].innerText = parseInt(parseInt((x[j].innerHTML) - 32) * (5 / 9));
+    }
+}
+
